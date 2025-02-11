18615f547d4d7f353314bf44804aed609da5207c
ThomasStock <thomasstock1985@gmail.com>
2025-02-11 22:41:25 +0100

    feat: read version from version.txt file

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,5 +1,8 @@
 import { type ReactNode } from "react";
 
+// Import version.txt as a string
+import version from "@/version.txt?raw";
+
 interface HeaderProps {
   leftButton?: ReactNode;
   rightButton?: ReactNode;
@@ -9,7 +12,7 @@ export function Header({ leftButton, rightButton }: HeaderProps) {
   return (
     <div className="flex items-center justify-between sticky top-0 pt-2 z-20 bg-background">
       {leftButton ?? <div />}
-      <span className="absolute left-1/2 -translate-x-1/2 text-xs text-muted-foreground">v{process.env.VERSION}</span>
+      <span className="absolute left-1/2 -translate-x-1/2 text-xs text-muted-foreground">v{version.trim()}</span>
       {rightButton ?? <div />}
     </div>
   );
