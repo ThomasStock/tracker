ea18c5cc849435d4c50b1facb4a827ba6415cadc
ThomasStock <thomasstock1985@gmail.com>
2025-02-14 21:57:47 +0100

    feat: add animated number flow to slider

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as SliderPrimitive from "@radix-ui/react-slider";
+import NumberFlow, { continuous } from "@number-flow/react";
 
 import { cn } from "@/lib/utils";
 
@@ -9,8 +10,14 @@ const Slider = React.forwardRef<React.ElementRef<typeof SliderPrimitive.Root>, R
       <SliderPrimitive.Track className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
         <SliderPrimitive.Range className="absolute h-full bg-primary" />
       </SliderPrimitive.Track>
-      <SliderPrimitive.Thumb className="block size-8 rounded-full border border-primary/50 bg-accent shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 flex items-center justify-center">
-        <span className="text-xs text-center">{props.value}</span>
+      <SliderPrimitive.Thumb className="size-8 rounded-full border border-primary/50 bg-accent shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 flex items-center justify-center">
+        <span className="text-xs text-center">
+          {props.value == undefined ? (
+            ""
+          ) : (
+            <NumberFlow spinTiming={{ duration: 250 }} isolate plugins={[continuous]} value={props.value[0]} />
+          )}
+        </span>
       </SliderPrimitive.Thumb>
     </SliderPrimitive.Root>
   )
